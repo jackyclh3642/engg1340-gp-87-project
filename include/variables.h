f977c0ba3df74ae804deed6cb837b1528f2f66cb
Jacky Chan <jackyclh3642@gmail.com>
2019-04-24 04:09:44 +0000

    more conflicts fixing

--- include/variables.h
+++ include/variables.h
@@ -3,7 +3,6 @@
 
 #include <string>
 
-<<<<<<< HEAD
 const std::string kAccountStrings[] = {
     "Cash",
     "Bank Card",
@@ -13,11 +12,6 @@ const std::string kAccountStrings[] = {
 const std::string kCatagoryStrings[] = {
     "Other Income",
     "Other Expense"
-=======
-const string kAccountStrings[] = { "Cash","Bank Card","Credit Card" };
-
-const string kCatagoryStrings = { "Expenses", "Revenue"    
->>>>>>> 9d7694ee4c71961764a6bc6de6285dc1a4486ff1
 };
 
 #endif
