c135922fea46fe192ed30e408ed07dc803038b6c
Lee <u3557410@hku.hk>
2019-04-25 09:27:57 +0800

    main.cpp updated and check_delete is made

--- new_main.cpp
+++ new_main.cpp
@@ -121,6 +121,92 @@ void editting_past_record(DaysDatabase map, int today){
 
 //function 2
 void editting_today_record(DaysDatabse map, int today){
+    bool continue_editting;
+    int correction_location;
+    Entry new_transaction_input;
+    while(continue_editting){
+        map->days[today]->Formatted();
+        cout<<"Input the number you want to edit"<<endl;
+        cin>>correction_location;
+        cout<<"Please input (account)[space](expenditure items/revenue items)[space][amount][space][remark]"<<endl;
+        cout<<"For account:"<<endl;
+        cout<<"Input 1 for Cash   account"<<endl;  
+        cout<<"Input 2 for Bank   account"<<endl;
+        cout<<"Input 3 for Credit account"<<endl;
+        cout<<"For expenditure items/revenue items"<<endl;
+        cout<<"Input  0 for Salary(revenue item)"<<endl;
+        cout<<"Input  1 for Other Income(revenue item)"<<endl;
+        cout<<"Input  2 for Entertainment(expenditure item)"<<endl;
+        cout<<"Input  3 for Transportation(expenditure item)"<<endl;
+        cout<<"Input  4 for Living Expense(expenditure item)"<<endl;
+        cout<<"Input  6 for Money transfer(expenditure item)"<<endl;
+        cout<<"Input  7 for Settle Debt(expenditure item)"<<endl;
+        cout<<"Input  8 for Fixed Expense(expenditure item)"<<endl;
+        cout<<"Input  9 for Rents(expenditure item)"<<endl;
+        cout<<"Input 10 for Other Expenditure(expenditure item)"<<endl;
+        cout<<"For amount:"<<endl;
+        cout<<"The amount should be positive when inputting revenue items, while the amount should be negative when inputting expenditure items"<<endl;
+        cout<<"For remark please enter a sentence to specify the transaction"<<endl;
+        cout<<"e.g. I using my cash account for transportation which cost 5 cents"<<endl;
+        cout<<"Please input 1 3 -0.5 I go to school by bus"<<endl;
+        cin >> new_transaction_input;
+        map->days[today]->transactions[correction_location - 1] = new_transaction_input;
+        bool correct_continue_input = true;
+        while(correct_continue_input){
+            cout<<"Do you want to continue edit? Input Y for yes meanwhile N for no"<<endl;
+            cin>> correct_continue_input;
+            if (input == 'Y')
+                correct_continue_input = false;
+                            else if(input == 'N')
+                return;
+            else
+                cout<<"Invalid input, please input again"<<endl;
+        }
+    }
+}
+////function appending past record
+void appending_past_record(DaysDatabase map, int today_index){
+    bool continue_edit_past_record = true;
+    Date past;
+    int past_index;
+    while (continue_edit_past_record){
+        cout<<"Please input the date before"<<map->days[today]<<"(Today) you want to edit in DD[space]MM[space]YY"<<endl;
+        cin >> past;
+        if (past > map->days[today]){   
+            Entry new_transaction_input;
+            past_index = map->FindDateIndex(past, 0, map->size);
+            Enrty Appending_transactions_input;
+            cout<<"Please input (account)[space](expenditure items/revenue items)[space][amount][space][remark]"<<endl;
+            cout<<"For account:"<<endl;
+            cout<<"Input 1 for Cash   account"<<endl;
+            cout<<"Input 2 for Bank   account"<<endl;
+            cout<<"Input 3 for Credit account"<<endl;
+            cout<<"For expenditure items/revenue items"<<endl;
+            cout<<"Input  0 for Salary(revenue item)"<<endl;
+            cout<<"Input  1 for Other Income(revenue item)"<<endl;
+            cout<<"Input  2 for Entertainment(expenditure item)"<<endl;
+            cout<<"Input  3 for Transportation(expenditure item)"<<endl;
+            cout<<"Input  4 for Living Expense(expenditure item)"<<endl;
+            cout<<"Input  6 for Money transfer(expenditure item)"<<endl;
+            cout<<"Input  7 for Settle Debt(expenditure item)"<<endl;
+            cout<<"Input  8 for Fixed Expense(expenditure item)"<<endl;
+            cout<<"Input  9 for Rents(expenditure item)"<<endl;
+            cout<<"Input 10 for Other Expenditure(expenditure item)"<<endl;
+            cout<<"For amount:"<<endl;
+            cout<<"The amount should be positive when inputting revenue items, while the amount should be negative when inputting expenditure items"<<endl;
+            cout<<"For remark please enter a sentence to specify the transaction"<<endl;
+            cout<<"e.g. I using my cash account for transportation which cost 5 cents"<<endl;
+            cout<<"Please input 1 3 -0.5 I go to school by bus"<<endl;
+            cin >> Appending_transaction_input;
+            map->days[past]<< Appending_transaction_input;
+            return;
+            
+        }
+        else{
+            cout<<"Invalid date is input;"
+        }
+    }    
+}
 
 
 
@@ -164,6 +250,7 @@ int main(){
                 cout<<"Input 1 for adding expenses/revenue records of today"<<endl;
                 cout<<"Input 2 for editing today record"<<endl;
                 cout<<"Input 3 for editing/adding past record"<<endl;
+                cout<<"Input 4 for adding past record"<<endl;
                 cout<<"Input 4 for checking daily record"<<endl;
                 cout<<"Input 5 for setting budget use in this month"<<endl;
                 cout<<"Input 6 for setting fix expenses and fix income "<<endl;
